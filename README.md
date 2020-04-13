7acba07278f3027c633560e0a7bffdf9c5007fda
Kasia Kittel <kkittel@equalexperts.com>
2020-04-13 14:32:21 +0100

    add cusomers data

---
+++ README.md
@@ -0,0 +1,18 @@
+Setup:
+```    
+export FLASK_APP=app
+export FLASK_ENV=development
+```
+
+Initialise the DB with hardcoded data:
+``` 
+flask init-db
+``` 
+    
+Run:
+``` 
+flask run
+``` 
+
+
+[How to install flask](https://flask.palletsprojects.com/en/1.1.x/installation/#installation)
--- app/schema.sql
+++ app/schema.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS consumer;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE consumers (
+  id INTEGER PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
+
+INSERT INTO consumers (id, username, password) VALUES (1, "Joe", "pass1");
+INSERT INTO consumers (id, username, password) VALUES (2, "Kate", "pass2");
