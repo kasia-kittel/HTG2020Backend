21413239706742062fbd5b6c718742233e5a2ea9
Kasia Kittel <kkittel@equalexperts.com>
2020-04-14 23:27:52 +0100

    separate db for tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,15 +1,33 @@
+import os
+import tempfile
+
 import pytest
 
+from app.db import init_db
 from app import create_app
 
 
 @pytest.fixture
 def app():
-    app = create_app()
+    """Create and configure a new app instance for each test."""
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+
+    # create the app with common test config
+    app = create_app({"TESTING": True, "DATABASE": db_path})
+
+    # create the database and load test data
+    with app.app_context():
+        init_db()
+
     yield app
+
+    # close and remove the temporary database
+    os.close(db_fd)
+    os.unlink(db_path)
 
 
 @pytest.fixture
 def client(app):
-    """A tests client for the app."""
+    """A test client for the app."""
     return app.test_client()
